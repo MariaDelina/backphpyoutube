998a32a59cb484827f0de3e8d787ff14c37f9b0b
MariaDelina <mdtrentinarl@gmail.com>
2023-11-28 20:29:37 -0300

    prueba koyeb 5

--- conexion.php
+++ conexion.php
@@ -2,15 +2,17 @@
 <?php
 
 class Conexion {
-    
+
     public function getConexion(){
-        $url = "jdbc:mysql://ubgb95dbdz9fiynn:HZODpPz7DDjGMCL0ERq4@byez16m96sq6rhn6fwec-mysql.services.clever-cloud.com:3306/byez16m96sq6rhn6fwec?useSSL=false&serverTimezone=UTC&useLegacyDateTimeCode=false"; // Reemplazar con la URL de conexión proporcionada por Clever Cloud
-        $user = "ubgb95dbdz9fiynn"; // Reemplazar con tu nombre de usuario de la base de datos
-        $password = "HZODpPz7DDjGMCL0ERq4"; // Reemplazar con tu contraseña de la base de datos
+        $host = "byez16m96sq6rhn6fwec-mysql.services.clever-cloud.com"; // Cambiar por la dirección del servidor de Clever Cloud
+        $port = "3306"; // Cambiar por el puerto de la base de datos en Clever Cloud
+        $db = "byez16m96sq6rhn6fwec"; // Cambiar por el nombre de la base de datos en Clever Cloud
+        $user = "ubgb95dbdz9fiynn"; // Cambiar por el nombre de usuario de la base de datos en Clever Cloud
+        $password = "HZODpPz7DDjGMCL0ERq4"; // Cambiar por la contraseña de la base de datos en Clever Cloud
 
         // conexión a la base de datos utilizando PDO
         try {
-            $db = new PDO("$url;user=$user;password=$password");
+            $db = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $password);
             return $db;
         } catch (PDOException $e) {
             // Manejar errores de conexión
